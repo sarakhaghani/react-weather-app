d66310c44ce8d388fa751a346dbfda22ea371451
sarakhaghani <sskkmilani@gmail.com>
2023-09-04 19:24:21 +0330

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Sara and is
+        <a
+          href="https://github.com/sarakhaghani/react-weather-app"
+          target="_blank"
+        >
+          {" "}
+          open-sorced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
